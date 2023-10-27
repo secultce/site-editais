09726f89f6fef7a7538c9b415680d33b904f5af5
Junior-Shyko <franciscaoanto@gmail.com>
2023-10-27 15:29:53 -0300

    :lipstick: Add menu
    Add icones e trocado os itens do menu

--- src/main.js
+++ src/main.js
@@ -2,6 +2,21 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
+//Terceiros
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { faBuildingColumns } from '@fortawesome/free-solid-svg-icons'
+import { faFolderOpen } from '@fortawesome/free-regular-svg-icons'
+import { faUserPen } from '@fortawesome/free-solid-svg-icons'
+/* add icons to the library */
+library.add(faPenToSquare,faBuildingColumns, faFolderOpen, faUserPen)
+
 
 // Nucleo Icons
 import "./assets/css/nucleo-icons.css";
@@ -14,4 +29,5 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.use(materialKit);
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount("#app");
--- src/views/Presentation/PresentationView.vue
+++ src/views/Presentation/PresentationView.vue
@@ -48,20 +48,10 @@ onUnmounted(() => {
 }
 </style>
 <template>
-  <div class="container position-sticky z-index-sticky top-0">
-    <div class="row">
-      <div class="col-12">
-        <DefaultNavbar
-          :action="{
-            route: 'javascript:;',
-            label: 'Buy Now',
-            color: 'btn-success',
-          }"
-          transparent
-        />
-      </div>
-    </div>
+  <div class="position-sticky  z-index-sticky top-0">
+    <NavbarDefault dark transparent />
   </div>
+  
   <Header>
     <div
       class="page-header min-vh-75"
