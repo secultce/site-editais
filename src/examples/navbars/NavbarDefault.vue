40700e76c949b6196b169b3153a80bf8f4f5dbd1
Junior-Shyko <franciscaoanto@gmail.com>
2023-11-07 14:21:49 -0300

    Alterando link para Router

--- src/examples/navbars/NavbarDefault.vue
+++ src/examples/navbars/NavbarDefault.vue
@@ -90,72 +90,42 @@ watch(
 );
 </script>
 <template>
-  <nav
-    class="navbar navbar-expand-lg top-0"
-    :class="{
-      'z-index-3 w-100 shadow-none navbar-transparent position-absolute my-3':
-        props.transparent,
-      'my-3 blur border-radius-lg z-index-3 py-2 shadow py-2 start-0 end-0 mx-4 position-absolute mt-4':
-        props.sticky,
-      'navbar-light bg-white py-3': props.light,
-      ' navbar-dark bg-gradient-dark z-index-3 py-3': props.dark
-    }"
-  >
-    <div
-      :class="
-        props.transparent || props.light || props.dark
-          ? 'container'
-          : 'container-fluid px-0'
-      "
-    >
-      <RouterLink
-        class="navbar-brand d-none d-md-block"
-        :class="[
-          (props.transparent && textDark.value) || !props.transparent
-            ? 'text-dark font-weight-bolder ms-sm-3'
-            : 'text-white font-weight-bolder ms-sm-3'
-        ]"
-        :to="{ name: 'presentation' }"
-        rel="tooltip"
-        title="Designed and Coded by Creative Tim"
-        data-placement="bottom"
-      >
+  <nav class="navbar navbar-expand-lg top-0" :class="{
+    'z-index-3 w-100 shadow-none navbar-transparent position-absolute my-3':
+      props.transparent,
+    'my-3 blur border-radius-lg z-index-3 py-2 shadow py-2 start-0 end-0 mx-4 position-absolute mt-4':
+      props.sticky,
+    'navbar-light bg-white py-3': props.light,
+    ' navbar-dark bg-gradient-dark z-index-3 py-3': props.dark
+  }">
+    <div :class="props.transparent || props.light || props.dark
+        ? 'container'
+        : 'container-fluid px-0'
+      ">
+      <RouterLink class="navbar-brand d-none d-md-block" :class="[
+        (props.transparent && textDark.value) || !props.transparent
+          ? 'text-dark font-weight-bolder ms-sm-3'
+          : 'text-white font-weight-bolder ms-sm-3'
+      ]" :to="{ name: 'presentation' }" rel="tooltip" title="Designed and Coded by Creative Tim"
+        data-placement="bottom">
         Secult/CE
       </RouterLink>
-      <RouterLink
-        class="navbar-brand d-block d-md-none"
-        :class="
-          props.transparent || props.dark
-            ? 'text-white'
-            : 'font-weight-bolder ms-sm-3'
-        "
-        to="/"
-        rel="tooltip"
-        title="Designed and Coded by Creative Tim"
-        data-placement="bottom"
-      >
-      Secult/CE
+      <RouterLink class="navbar-brand d-block d-md-none" :class="props.transparent || props.dark
+          ? 'text-white'
+          : 'font-weight-bolder ms-sm-3'
+        " to="/" rel="tooltip" title="Designed and Coded by Creative Tim" data-placement="bottom">
+        Secult/CE
       </RouterLink>
-      
-      <button
-        class="navbar-toggler shadow-none ms-2"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navigation"
-        aria-controls="navigation"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
+
+      <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation"
+        aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon mt-2">
           <span class="navbar-toggler-bar bar1"></span>
           <span class="navbar-toggler-bar bar2"></span>
           <span class="navbar-toggler-bar bar3"></span>
         </span>
       </button>
-      <div
-        class="collapse navbar-collapse w-100 pt-3 pb-2 py-lg-0"
-        id="navigation"
-      >
+      <div class="collapse navbar-collapse w-100 pt-3 pb-2 py-lg-0" id="navigation">
         <ul class="navbar-nav navbar-nav-hover ms-auto">
           <!-- <li class="nav-item dropdown dropdown-hover mx-2">
             <a
@@ -905,52 +875,43 @@ watch(
             </div>
           </li> -->
           <li class="nav-item dropdown dropdown-hover mx-2">
-            <a
-              href="#"
-              class="nav-link d-flex cursor-pointer align-items-center"
-            >
-            <font-awesome-icon icon="fa-solid fa-pen-to-square" style="color: #d6d8dc;" class="p-2"/>
-              Editais
+            <a class="nav-link d-flex cursor-pointer align-items-center">
+              <font-awesome-icon icon="fa-solid fa-pen-to-square" style="color: #d6d8dc;" class="p-2" />
+              <RouterLink :to="{ name: 'presentation' }" class="dropdown-item border-radius-md">
+                <span>Editais</span>
+              </RouterLink>
             </a>
           </li>
           <li class="nav-item dropdown dropdown-hover mx-2">
-            <a
-              href="#"
-              class="nav-link d-flex cursor-pointer align-items-center"
-            >
-            <font-awesome-icon icon="fa-solid fa-building-columns"  style="color: #d6d8dc;" class="p-2"/>
-              Equipamentos
-            </a>
-          </li>
-           <li class="nav-item dropdown dropdown-hover mx-2">
-            <a
-              href="#"
-              class="nav-link d-flex cursor-pointer align-items-center"
-            >
-            <font-awesome-icon icon="fa-regular fa-folder-open"  style="color: #d6d8dc;" class="p-2"/>
-              Serviços
+            <a class="nav-link d-flex cursor-pointer align-items-center">
+              <font-awesome-icon icon="fa-solid fa-building-columns" style="color: #d6d8dc;" class="p-2" />
+              <RouterLink :to="{ name: 'equipamentos' }" class="dropdown-item border-radius-md">
+                <span>Equipamentos</span>
+              </RouterLink>
             </a>
           </li>
           <li class="nav-item dropdown dropdown-hover mx-2">
-            <a
-              href="#"
-              class="nav-link d-flex cursor-pointer align-items-center"
-            >
-            <font-awesome-icon icon="fa-solid fa-user-pen"  style="color: #d6d8dc;" class="p-2"/>
-            CEFIC
+            <a class="nav-link d-flex cursor-pointer align-items-center">
+              <font-awesome-icon icon="fa-regular fa-folder-open" style="color: #d6d8dc;" class="p-2" />
+              <RouterLink :to="{ name: 'about' }" class="dropdown-item border-radius-md">
+                <span>Serviços</span>
+              </RouterLink>
             </a>
           </li>
-          
+          <li class="nav-item dropdown dropdown-hover mx-2">
+            <a href="cefic" class="nav-link d-flex cursor-pointer align-items-center">
+              <font-awesome-icon icon="fa-solid fa-user-pen" style="color: #d6d8dc;" class="p-2" />
+              <RouterLink :to="{ name: 'contactus' }" class="dropdown-item border-radius-md">
+                <span>CEFIC</span>
+              </RouterLink>
+            </a>
+          </li>
+
         </ul>
         <ul class="navbar-nav d-lg-block d-none">
           <li class="nav-item">
-            <a
-              :href="action.route"
-              class="btn btn-sm mb-0"
-              :class="action.color"
-              onclick="smoothToPricing('pricing-soft-ui')"
-              >{{ action.label }}</a
-            >
+            <a :href="action.route" class="btn btn-sm mb-0" :class="action.color"
+              onclick="smoothToPricing('pricing-soft-ui')">{{ action.label }}</a>
           </li>
         </ul>
       </div>
---
+++ src/views/LandingPages/Equipment/Equipment.vue
@@ -0,0 +1,152 @@
+<script setup>
+import Header from "../../../examples/Header.vue";
+import NavbarDefault from "../../../examples/navbars/NavbarDefault.vue";
+import AboutUsOption from '../components/AboutUsOption.vue'
+
+//images
+import vueMkHeader from "@/assets/img/header-ceara.png";
+import { reactive } from "vue";
+
+const linkEquipments = [
+  {
+    title: 'Arquivo Público Estadual do Ceará',
+    link: 'https://mapacultural.secult.ce.gov.br/espaco/499/'
+  },
+  {
+    title: 'Casa de Juvenal Galeno',
+    link: 'https://mapacultural.secult.ce.gov.br/espaco/490/'
+  },
+  {
+    title: 'Centro Cultural Bom Jardim',
+    link: 'https://ccbj.org.br/'
+  },
+  {
+    title: 'Centro Dragão do Mar de Arte e Cultura',
+    link: 'http://www.dragaodomar.org.br/'
+  },
+  {
+    title: 'Escola de Artes e Ofícios Thomaz Pompeu Sobrinho',
+    link: 'http://eao.org.br/'
+  },
+  {
+    title: 'Museu de Arte Contemporânea do Ceará',
+    link: 'http://www.dragaodomar.org.br/espacos/museu-de-arte-contemporanea'
+  },
+  {
+    title: 'Museu da Cultura Cearense',
+    link: 'http://www.dragaodomar.org.br/espacos/museu-da-cultura-cearense'
+  },
+  {
+    title: 'Museu da Imagem e do Som',
+    link: 'https://mapacultural.secult.ce.gov.br/espaco/265/'
+  },
+  {
+    title: 'Teatro Carlos Câmara',
+    link: 'https://mapacultural.secult.ce.gov.br/espaco/485/'
+  },
+  {
+    title: 'Porto Iracema das Artes',
+    link: 'https://portoiracemadasartes.org.br/'
+  },
+  {
+    title: 'Biblioteca Pública do Estado do Ceará',
+    link: 'http://www.bece.cultura.ce.gov.br/'
+  },
+  {
+    title: 'Casa de Saberes Cego Aderaldo – Quixadá',
+    link: 'https://mapacultural.secult.ce.gov.br/espaco/891/'
+  },
+  {
+    title: 'Centro Cultural Porto Dragão',
+    link: 'https://mapacultural.secult.ce.gov.br/espaco/1632/'
+  },
+  {
+    title: 'Cineteatro São Luiz',
+    link: 'https://www.cineteatrosaoluiz.com.br/'
+  },
+  {
+    title: 'Escola de Gastronomia Social Ivens Dias Branco',
+    link: 'https://gastronomiasocial.org.br/'
+  },
+  {
+    title: 'Museu do Ceará',
+    link: 'https://mapacultural.secult.ce.gov.br/espaco/87/'
+  },
+  {
+    title: 'Museu Sacro São José de Ribamar',
+    link: 'https://mapacultural.secult.ce.gov.br/espaco/539/'
+  },
+  {
+    title: 'Sobrado Dr. José Lourenço',
+    link: 'https://mapacultural.secult.ce.gov.br/espaco/305/'
+  },
+  {
+    title: 'Theatro José de Alencar',
+    link: 'https://mapacultural.secult.ce.gov.br/espaco/317/'
+  },
+  {
+    title: 'Vila da Música – Crato',
+    link: 'https://mapacultural.secult.ce.gov.br/espaco/1499/'
+  },
+
+]
+
+
+</script>
+
+
+<template>
+  <div class="position-sticky  z-index-sticky top-0">
+    <NavbarDefault dark transparent />
+  </div>
+  <Header>
+    <div class="page-header min-vh-75" :style="`background-image: url(${vueMkHeader})`" loading="lazy" style="">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-7 text-center mx-auto position-relative">
+            <!-- <h1
+              class="text-white pt-3 mt-n5 me-2"
+              :style="{ display: 'inline-block ' }"
+            >
+              Material Kit 2
+            </h1>
+            <p class="lead text-white px-5 mt-3" :style="{ fontWeight: '500' }">
+              Start the Development With Bootstrap 5 Design System inspired by
+              Material Design.
+            </p> -->
+          </div>
+        </div>
+      </div>
+    </div>
+    <section class="py-4">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <h3 class="border-bottom border-success">Rede de Equipamentos Culturais</h3>
+            <p class="pe-5 py-3">
+              Para democratizar, fomentar e fazer circular as artes no Ceará, além de proporcionar a
+              formação e a profissionalização para diversas áreas, a Secult mantém uma rede de equipamentos culturais na
+              capital e
+              no interior do Estado. O Instituto Dragão do Mar contribui com a gestão de equipamentos como o Centro Dragão
+              do Mar
+              de Arte e Cultura, o Centro Cultural Grande Bom Jardim,
+              a Escola Porto Iracema das Artes, entre outros, fortalecendo esta rede estadual.
+            </p>
+
+          </div>
+          <div class="col-lg-6 mt-lg-0 mt-5 ps-lg-0 ps-0">
+            <h3 class="border-bottom border-success">Links de acesso</h3>
+            <ul class="list-group" v-for="(item, index) in linkEquipments">
+              <a :href="item.link" :key="index" class="list-group-item list-group-item-action">
+                <i class="fa fa-check"></i> {{ item.title }}
+              </a>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  </Header>
+</template>
+
+
+<style lang="scss" scoped></style>
